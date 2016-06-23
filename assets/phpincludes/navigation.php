bd1753dca570aa36b11a5a6560514260a52e5621
Thorin Butler <thorin.butler@gmail.com>
2016-06-23 18:34:42 -0500

    added some flex box bits and some styling

--- assets/phpincludes/navigation.php
+++ assets/phpincludes/navigation.php
@@ -5,13 +5,10 @@ $articles = array("Journal" => "journal.php");
 $profiles = array("twitter" => "https://twitter.com/ThinkingBinary", "linkedin" => "https://www.linkedin.com/in/thorinbutler", "github" => "https://github.com/tsbutler");
 ?> 
 
-<ul>
   <?php foreach($profiles as $key => $val) { ?>
-    <li>
-      <a href="<?php echo $val; ?>">
-        <img src="../images/<?php echo $key; ?>.png" alt="<?php echo $key; ?>">
-      </a>
-    </li>
+    <a href="<?php echo $val; ?>">
+      <img src="assets/images/<?php echo $key; ?>.png" alt="<?php echo $key; ?>" style= "width:64px;height:64px;border:0;">
+    </a>
   <?php } ?>
 </ul>
 <p>Pages</p>
