6be7edae3f53b85d79a9a544c30ac4f6eedaf11d
Thorin Butler <thorin.butler@gmail.com>
2016-06-27 15:14:05 -0500

    added a page where the TTT and RPS games will eventually go

---
+++ games.php
@@ -0,0 +1,25 @@
+<?php $thisPage="Games"; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+    <title>Things</title>
+  </head>
+  <body>
+    <header><?php echo $thisPage ?></header>
+      <div id='main'>
+        <nav>
+          <div class="nav-bar">
+            <?php include 'assets/phpincludes/navigation.php';?>
+          </div>
+        </nav>
+        <article>
+          <h1></h1>
+            <h2>What this is...</h2>
+              <p>This portion of the page showcases a few simple games you can play.</p>
+        </article>
+        <aside></aside>
+      </div>
+    <footer></footer>
+  </body>
+</html>
